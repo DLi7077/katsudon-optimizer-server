6e0b8b482ec9844729083b67da181f062a8bae90
butterriolu <derpvin12@gmail.com>
2022-12-13 03:25:57 -0500

    add dotenv

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
   console.log(`Express listening at port ${port}`);
   console.log(`Test apis with http://localhost:${port}/api`);
 });
 
-setInterval(() => {
-  runOptimizer();
-}, 1000);
+// setInterval(() => {
+//   runOptimizer();
+// }, 1000);
