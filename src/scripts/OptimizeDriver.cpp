9fe7417a55e4f4e2245f0e91bb77f9e410a9fc6f
butterriolu <derpvin12@gmail.com>
2022-12-20 17:48:30 -0500

    add support for double type

--- src/scripts/OptimizeDriver.cpp
+++ src/scripts/OptimizeDriver.cpp
@@ -1,5 +1,5 @@
 #include "./Constants/pch.h"
-#include "./JSONParser/JSONParser.cpp"
+#include "./JSON/JSONObject.cpp"
 #include "./Utils/optimize.cpp"
 
 using namespace std;
@@ -10,30 +10,25 @@ int main(int argc, char** argv) {
     return 0;
   }
 
-  std::string jsonObject = argv[1];
+  std::string arg1 = argv[1];
   double flatATK = std::stod(argv[2]);
-  JSONParser::createBonusStatGains(jsonObject);
-  JSONParser::createStatValues(jsonObject);
 
-  // LOG("{\n");
-  // for (auto p : JSONParser::stringValues) {
-  //   LOG("\"" << p.first << "\""
-  //            << ": "
-  //            << "\"" << p.second << "\""
-  //            << ",");
-  // }
-  // for (auto p : JSONParser::doubleValues) {
-  //   LOG("\"" << p.first << "\""
-  //            << ": " << p.second << ",");
-  // }
-  // LOG("\"bonus_stat_gains\": [\n")
-  // for (size_t i = 0; i < JSONParser::bonusStatGains.size(); i++) {
-  //   LOG(JSONParser::bonusStatGains[i] << (i == JSONParser::bonusStatGains.size() - 1 ? "" : ","));
-  // }
-  // LOG("]\n");
-  // LOG("}\n");
+  Json::JsonObject input(arg1);
+  LOG(input);
 
   Character character(PYRO);
+  character.setStat(BASE_ATK, 700);
+  character.setStat(CRIT_DAMAGE, DEFAULT_CRIT_DAMAGE + .1);
+  character.setStat(FLAT_ATK, flatATK);
+  character.setStat(ATK_PERCENT, 46.6);
+  character.setStat(FLAT_HP, 311);
+  character.setStat(HP_PERCENT, 46.6);
+  character.setStat(FLAT_DEF, 311);
+  character.setStat(DEF_PERCENT, 46.6);
+  character.setStat(ELEMENTAL_MASTERY, 238);
+  character.setStat(ENERGY_RECHARGE, 2.0);
+  character.setStat(MELT_BONUS, .15);
+  
   character.setStat(BASE_ATK, 700);
   character.setStat(CRIT_DAMAGE, DEFAULT_CRIT_DAMAGE + .1);
   character.setStat(FLAT_ATK, flatATK);
@@ -56,7 +51,7 @@ int main(int argc, char** argv) {
   enemy.setLevel(90);
   enemy.setResistance(PYRO, -1.8);
 
-  Optimize::optimize(character, enemy);
+  // Optimize::optimize(character, enemy);
 
   /**
    * TODO:
