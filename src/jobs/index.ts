88488d109f245f49cb6bfe129b7ba6d850801374
Devin Li <51217000+DLi7077@users.noreply.github.com>
2022-12-24 00:03:18 -0500

    add description to service functions

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,7 +1,8 @@
 import cron from "node-cron";
 import async, { AsyncFunction, AsyncResultCallback } from "async";
 import _ from "lodash";
-import OptimizeService from "../services/Optimize";
+import OptimizeService from "../services/optimize-result";
+import RequestService from "../services/optimize-requests";
 import { OptimizeRequestAttributes } from "../database/models/OptimizeRequest";
 import { ObjectId } from "mongoose";
 
@@ -26,13 +27,14 @@ function createGenerateResultTasks(
 
 // https://github.com/node-cron/node-cron
 function schedule() {
-  const everyThreeSeconds = `*/3 * * * * *`;
+  const secondInterval = 5;
+  const everyThreeSeconds = `*/${secondInterval} * * * * *`;
   cron.schedule(everyThreeSeconds, () => {
     console.log(`running ${processCount} processes at`, new Date());
     if (processCount == PROCESS_LIMIT) return;
 
     const remainingBandwidth = PROCESS_LIMIT - processCount;
-    OptimizeService.getPendingRequests(remainingBandwidth).then(
+    RequestService.getPendingRequests(remainingBandwidth).then(
       (requests: OptimizeRequestAttributes[]) => {
         processCount += requests.length;
         const requestIds = _.map(
---
+++ src/services/optimize-result.ts
@@ -0,0 +1,63 @@
+import _ from "lodash";
+import Models from "../database";
+import { OptimizeRequestAttributes } from "../database/models/OptimizeRequest";
+import { ObjectId } from "mongoose";
+import { runOptimizer } from "../utils";
+import { OptimizeResultAttributes } from "../database/models/OptimizeResult";
+import { NotFoundResponse } from "http-errors-response-ts/lib";
+
+import RequestService from "./optimize-requests";
+
+/**
+ * @description returns a request document
+ * @param {ObjectId} resultId - the result_id to check for
+ * @returns {Promise<OptimizeResultAttributes>} Promise of the fetched result
+ */
+async function findResultById(
+  resultId: ObjectId
+): Promise<OptimizeResultAttributes | any> {
+  const exists = await Models.OptimizeResult.exists({ _id: resultId });
+  if (!exists) {
+    const error = new NotFoundResponse("No Object Id found");
+    return error;
+  }
+
+  return Models.OptimizeResult.findById(resultId);
+}
+
+/**
+ * @description runs the optimizer algorithm script on the request
+ * creates a result document after successful computation
+ * @param {ObjectId} requestId - the request_id document to compute
+ * @returns {Promise<OptimizeResultAttributes>} Promise of a created result document
+ */
+async function generateResult(
+  requestId: ObjectId
+): Promise<OptimizeResultAttributes> {
+  const foundRequest = (await Models.OptimizeRequest.findById(
+    requestId
+  )) as OptimizeRequestAttributes;
+
+  await RequestService.setRequestProcessing(requestId);
+  const computedResult: OptimizeResultAttributes = await runOptimizer(
+    foundRequest
+  );
+
+  const generatedResult: OptimizeResultAttributes =
+    await Models.OptimizeResult.create({
+      ...computedResult,
+      request_id: requestId,
+    });
+
+  await RequestService.setRequestComplete(
+    requestId,
+    _.get(generatedResult, "_id")
+  );
+
+  return generatedResult;
+}
+
+export default {
+  findResultById,
+  generateResult,
+};
