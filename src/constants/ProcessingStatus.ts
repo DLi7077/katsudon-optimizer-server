63369ef37095346ea3c5ed878ea0032d421ca721
butterriolu <derpvin12@gmail.com>
2022-12-28 22:48:53 -0500

    add error status

--- src/constants/ProcessingStatus.ts
+++ src/constants/ProcessingStatus.ts
@@ -2,6 +2,11 @@ export enum STATUS {
   PENDING = "pending",
   PROCESSING = "processing",
   COMPLETE = "complete",
+  ERROR = "error",
 }
 
-export type Status = STATUS.PENDING | STATUS.PROCESSING | STATUS.COMPLETE;
+export type Status =
+  | STATUS.PENDING
+  | STATUS.PROCESSING
+  | STATUS.COMPLETE
+  | STATUS.ERROR;
